5feea31f99e65b98659950c223fa26570a274bcf
VINAY MITTAL <vinay@vinsol.com>
2015-06-22 14:06:28 +0530

    nothing

--- company_name_change.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#file containing company names and addresses is named as company_name
-#stream editor used to edit lines and basic search and replace is used
-#single quotes are not used and double quotes are used to differentiate two strings and make use of backslash as it loses its special power in   single quotes and backslash is used to define special character as it was necessary to enter "//" ahead of websites 
-cat company_name | sed s/"www\."/"http:\/\/www."/
-#now all the edited company names are displayed
-cat company_name
